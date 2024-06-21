32b035155bd963b0cbe86c253a9362435ba620b6
Hieu Bui <hieubv1306@gmail.com>
2024-06-21 23:37:43 +0700

    Install necessary package for project

--- BackEnd/BE-E-Commerce/obj/Debug/net8.0/BE-E-Commerce.AssemblyInfo.cs
+++ BackEnd/BE-E-Commerce/obj/Debug/net8.0/BE-E-Commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE-E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9906c6c01e464e05d51200192b00a94c6f0af173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE-E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE-E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
